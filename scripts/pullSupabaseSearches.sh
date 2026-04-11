d599bb55cc5b572317f0282643d1613ed6234ed6
ben20russell <ben20russell@gmail.com>
2026-04-10 22:09:10 -0700

    Remove continuous Supabase data pull scripts

--- scripts/pullSupabaseSearches.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Run the Supabase pull script every 5 minutes forever
-while true; do
-  VITE_SUPABASE_ANON_KEY="$VITE_SUPABASE_ANON_KEY" node ./pullSupabaseSearches.js
-  sleep 300
-done
